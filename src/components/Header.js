e17da6d595116babb75001266499dca7a8ff0515
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-07 22:01:19 +0200

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,51 @@
-import { AppBar, Select, Toolbar, Typography, Container, MenuItem } from '@mui/material'
+import { AppBar, Select, Toolbar, Typography, Container, MenuItem, createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
+import { makeStyles } from 'tss-react/mui';
+import { useNavigate } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
 
+const useStyles = makeStyles()(() => {
+  return {
+    title: {
+      flex: 1,
+      color: 'gold',
+      fontFamily: 'Montserrat',
+      fontWeight: 'bold',
+      cursor: 'pointer'
+    },
+  };
+})
 
 const Header = () => {
+
+  const { classes } = useStyles();
+  const navigate = useNavigate()
+
+  const { currency, setCurrency } = CryptoState()
+  console.log(currency)
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
-    <AppBar color='transparent' position='static'>
-      <Container>
-        <Toolbar>
-          <Typography>
-            CryptoTracker
-          </Typography>
-          <Select variant='outlined' style={{ width: 100, height: 40, marginLeft: 15 }}>
-            <MenuItem value={'EUR'}>EUR</MenuItem>
-            <MenuItem value={'USD'}>USD</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography onClick={() => navigate('/')} className={classes.title} variant='h5'>
+              CryptoTracker
+            </Typography>
+            <Select variant='outlined' style={{ width: 100, height: 40, marginRight: 15 }} value={currency} onChange={(e) => setCurrency(e.target.value)}>
+              <MenuItem value={'EUR'}>EUR</MenuItem>
+              <MenuItem value={'USD'}>USD</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   )
 }
 
